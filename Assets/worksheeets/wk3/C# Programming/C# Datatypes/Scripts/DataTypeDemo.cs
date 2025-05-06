cffd3b435b6f90d5d35f56c16a1a3ca82a662d20
rena <lminge007@gmail.com>
2025-05-06 09:40:40 +0800

    wk 3 code
    scripts

---
+++ Assets/worksheeets/wk3/C# Programming/C# Datatypes/Scripts/DataTypeDemo.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace ProjectNamespace1
+{
+
+    public class DataTypeDemo : MonoBehaviour
+    {
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

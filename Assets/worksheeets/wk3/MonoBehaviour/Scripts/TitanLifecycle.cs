bc49119dd7e5b11a07ea70cdba5861bc9c890c48
rena <lminge007@gmail.com>
2025-06-23 18:35:54 +0800

    wk 3 worrksheet
    monobehaviour doneee

---
+++ Assets/worksheeets/wk3/MonoBehaviour/Scripts/TitanLifecycle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TitanLifecycle : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Titan-01: Initial boot sequence initiated.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Titan-01: Power core stable. Ready for action.");
+    }
+
+    void Update()
+    {
+        Debug.Log("Titan-01: Scanning for threats...");
+    }
+}
+
